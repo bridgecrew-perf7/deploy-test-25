2fc0031c8ac362a685e4e44e0af343b6cf489e96
Filippo Menis <filippomeniswork@gmail.com>
2021-11-30 09:56:34 +0100

    base server

---
+++ index.js
@@ -0,0 +1,19 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: {
+    level: "debug",
+  },
+});
+
+fastify.get("/hello", async (req, reply) => {
+  return "Hello World!";
+});
+
+fastify.listen(3000, "0.0.0.0", (err) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.debug(`Server up and running at port 3000`);
+});
