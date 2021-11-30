27461d6ba61ab2b772868509e7f82fb1cd89721f
Filippo Menis <filippomeniswork@gmail.com>
2021-11-30 10:45:23 +0100

    add ecosystem file

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ const fastify = Fastify({
 });
 
 fastify.get("/hello", async (req, reply) => {
-  return "Hello World! --> pm2 restart";
+  return { msg: "Hello World!", v: 2 };
 });
 
-fastify.listen(3000, "0.0.0.0", (err) => {
+const port = 3000;
+const host = "0.0.0.0";
+
+fastify.listen(port, host, (err) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
-  fastify.log.debug(`Server up and running at port 3000`);
+  fastify.log.debug(`Server up and running in ${process.env.NODE_ENV} mode`);
 });
